fdb06fde49cab9e36f6eb843ca74d6784f63d882
bubba-94 <carljohan.modin@gmail.com>
2025-10-19 16:18:08 +0200

    New strucutre. KEEP GOING

--- Practical-C-O-Reilly/8-chapter-more-control-statements/ex1.c
+++ Practical-C-O-Reilly/8-chapter-more-control-statements/ex1.c
@@ -6,14 +6,15 @@
 */
 
 int main(){
-    for (int i = 0; i < 2; i ++){
-        for(int j = 0; j < 5; j++){
+    for (int i = 0; i < 8; i ++){
+        for(int j = 0; j < 1; j++){
             printf("+-----\n");
-            for (int k = 0; k < 3; k++){
-                if (k != 0 || k != 3) printf("|\n");
+            for (int k = 0; k < 2; k++){
+                if ()
+                printf("|\n");
+                
             }
         }
-        printf("+-----\n");
     }
     return 0;
 }
